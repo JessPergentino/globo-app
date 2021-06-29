a9d8e964c1380dd015c881b786a79127c82cdd60
Jessica Pergentino <jessicapergentino@hotmail.com>
2021-06-29 20:58:30 -0300

    Setup Routing, Header and Footer

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from './Home';
+import { Platform, StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import Header from './Header';
+import Footer from './Footer';
+import { navigationRef } from './RootNavigation';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf')
+  })
+  if (!fontsLoaded) {
+    return (<AppLoading />)
+  } else {
+    return (
+      <NavigationContainer
+        style={{ paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0 }}
+        ref={navigationRef}
+      >
+        <Stack.Navigator
+          initialRouteName="Globomantics"
+          headerMode="screen"
+        >
+          <Stack.Screen
+            name="Globomantics"
+            component={HomePage}
+            options={{
+              header: () => <Header headerDisplay="Globomantics" />
+            }}
+          />
+        </Stack.Navigator>
+        <Footer />
+      </NavigationContainer>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
